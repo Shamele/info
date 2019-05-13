ef4043706e5f65e018e53394b71590b2dc349d59
shamele <shamele024@gmail.com>
2019-05-13 15:22:26 +0530

    a

---
+++ Companies.java
@@ -0,0 +1,18 @@
+package org.comp;
+
+
+public class Companies {
+	public void clientName() {
+		// TODO Auto-generated method stub
+		System.out.println("sanjesh");
+
+	}
+	public static void main(String[] args) {
+		Companies co=new Companies();
+		co.clientName();
+		
+		
+	 	
+	
+	}
+}
